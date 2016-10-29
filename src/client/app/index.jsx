50633085f5a5b18fe7be0f47fa315621510b1e9e
jukhan <jasim.uddin.khan@gmail.com>
2016-10-29 12:45:56 +0600

    datatable

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -6,6 +6,7 @@ import Home from './Home.jsx';
 import Nope from './Nope.jsx';
 import Game from './Game.jsx';
 import Game2 from './Game2.jsx';
+import{DataTable} from './DataTable.jsx';
 
 class App extends React.Component {
   render () {
@@ -15,7 +16,8 @@ class App extends React.Component {
               <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
               <li><Link to="/awesome" activeClassName="active">Awesome</Link></li>
               <li><Link to="/game" activeClassName="active">Game</Link></li> 
-              <li><Link to="/game2" activeClassName="active">Game2</Link></li>               
+              <li><Link to="/game2" activeClassName="active">Game2</Link></li>
+              <li><Link to="/datatable" activeClassName="active">DataTable</Link></li>               
             </ul>
             <div className="content">
               {this.props.children}
@@ -30,7 +32,8 @@ render(
           <IndexRoute component={Home} />
           <Route path="awesome" component={AwesomeComponent} />  
           <Route path="game" component={Game} /> 
-          <Route path="game2" component={Game2} />        
+          <Route path="game2" component={Game2} />   
+          <Route path="datatable" component={DataTable} />     
           <Route path='*' component={Nope} />
         </Route>
 
